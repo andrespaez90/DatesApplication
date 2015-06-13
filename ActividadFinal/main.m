c35768a2209275b76a48304ef252afc1f4646d49
INNOVATING SOFTWARE SAS <INNSO@iMac-de-INNOVATING.local>
2015-06-13 17:27:37 -0500

    Initial Commit

---
+++ ActividadFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ActividadFinal
+//
+//  Created by INNOVATING SOFTWARE SAS on 13/06/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ActividadFinalTests/ActividadFinalTests.m
@@ -0,0 +1,40 @@
+//
+//  ActividadFinalTests.m
+//  ActividadFinalTests
+//
+//  Created by INNOVATING SOFTWARE SAS on 13/06/15.
+//  Copyright (c) 2015 INNOVATING SOFTWARE SAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ActividadFinalTests : XCTestCase
+
+@end
+
+@implementation ActividadFinalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
